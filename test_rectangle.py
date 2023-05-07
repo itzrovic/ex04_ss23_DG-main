395877482b4a536eb986fad3f5cac31bebd835f2
itzrovic <120782777+itzrovic@users.noreply.github.com>
2023-05-07 16:51:08 +0200

    imported testcases from README -> 4 testcases: OK

--- test_rectangle.py
+++ test_rectangle.py
@@ -16,6 +16,23 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(30, self.sut1.calc_perimeter())
         self.assertEqual(80, self.sut2.calc_perimeter())
 
+    # imported from README
+    def test_equal_rectangle(self):
+        r1 = Rectangle(3, 4)
+        r2 = Rectangle(4, 3)
+        r3 = Rectangle(3, 4)
+
+        self.assertEqual(r1, r3)
+        self.assertNotEqual(r1, r2)
+
+    def test_hash_rectangle(self):
+        r1 = Rectangle(3, 4)
+        r2 = Rectangle(4, 3)
+        r3 = Rectangle(3, 4)
+
+        self.assertEqual(hash(r1), hash(r3))
+        self.assertNotEqual(hash(r1), hash(r2))
+
 
 if __name__ == '__main__':
     unittest.main()
