4cc4a65961ffe17abbec60aed7fa1dde623b0fea
itzrovic <120782777+itzrovic@users.noreply.github.com>
2023-05-07 16:36:15 +0200

    changed length to width

--- rectangle.py
+++ rectangle.py
@@ -1,10 +1,10 @@
 class Rectangle:
-    def __init__(self, length: int, height: int):
-        self._length = length
+    def __init__(self, width: int, height: int):
+        self._width = width
         self._height = height
 
     def calc_area(self) -> int:
-        return self._length * self._height
+        return self._width * self._height
 
     def calc_perimeter(self) -> int:
-        return 2 * self._length + 2 * self._height
+        return 2 * self._width + 2 * self._height
