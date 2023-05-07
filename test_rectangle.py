97b3a5aeecd8d0617720c9f4268414dcdd0ecd34
itzrovic <120782777+itzrovic@users.noreply.github.com>
2023-05-07 17:48:37 +0200

    added testcases for methods add and remove -> result: OK

--- test_rectangle.py
+++ test_rectangle.py
@@ -1,12 +1,13 @@
 import unittest
 
-from rectangle import Rectangle
+from rectangle import Rectangle, RectangleList
 
 
 class TestRectangle(unittest.TestCase):
     def setUp(self) -> None:
         self.sut1 = Rectangle(5, 10)
         self.sut2 = Rectangle(23, 17)
+        self.sut3 = Rectangle(6, 9)
 
     def test_calc_area(self):
         self.assertEqual(50, self.sut1.calc_area())
@@ -32,6 +33,29 @@ class TestRectangle(unittest.TestCase):
 
         self.assertEqual(hash(r1), hash(r3))
         self.assertNotEqual(hash(r1), hash(r2))
+
+    # testcases for RectangleList
+    def test_rect_list_add(self):
+        rl = RectangleList()
+
+        self.assertEqual(len(rl), 0)
+        rl.add(self.sut1)
+        self.assertEqual(len(rl), 1)
+        rl.add(self.sut2)
+        self.assertEqual(len(rl), 2)
+
+    def test_rect_list_remove(self):
+        rl = RectangleList()
+
+        rl.add(self.sut1)
+        rl.add(self.sut2)
+        self.assertEqual(len(rl), 2)
+
+        self.assertTrue(rl.includes(self.sut2))
+        self.assertFalse(rl.includes(self.sut3))
+        rl.remove(self.sut2)
+        self.assertFalse(rl.includes(self.sut2))
+        self.assertRaises(ValueError, rl.remove, self.sut3)
 
 
 if __name__ == '__main__':
