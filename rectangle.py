9451e2c6768dc596ce53163eb85c4dba95ec5725
itzrovic <120782777+itzrovic@users.noreply.github.com>
2023-05-07 16:49:59 +0200

    added __eq__, __hash__ and a new class RectangleList

--- rectangle.py
+++ rectangle.py
@@ -8,3 +8,28 @@ class Rectangle:
 
     def calc_perimeter(self) -> int:
         return 2 * self._width + 2 * self._height
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return (self._width, self._height) == (other._width, other._height)
+        return False
+
+    def __hash__(self):
+        return hash((self._width, self._height))
+
+
+class RectangleList:
+    def __init__(self):
+        self._rectangles = []
+
+    def __len__(self) -> int:
+        return len(self._rectangles)
+
+    def add(self, r: Rectangle):
+        self._rectangles.append(r)
+
+    def remove(self, r: Rectangle):
+        self._rectangles.remove(r)
+
+    def includes(self, r) -> bool:
+        return r in self._rectangles
