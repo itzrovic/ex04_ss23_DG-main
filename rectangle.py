912485f725bfaa498d4766e3eb5f7a57d71e3108
itzrovic <120782777+itzrovic@users.noreply.github.com>
2023-05-07 16:05:09 +0200

    Step 1.2 done

--- rectangle.py
+++ rectangle.py
@@ -1,2 +1,7 @@
 class Rectangle:
-    pass
+    def __init__(self, width: int, height: int):
+        self._width = width
+        self._height = height
+
+    def calc_perimeter(self) -> int:
+        return 2 * self._width + 2 * self._height
