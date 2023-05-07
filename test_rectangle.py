9f1db1f7a4d845b15601379e0aed378ec75274e8
itzrovic <120782777+itzrovic@users.noreply.github.com>
2023-05-07 16:20:44 +0200

    test_rectangle: implemented testcases

--- test_rectangle.py
+++ test_rectangle.py
@@ -4,7 +4,13 @@ from rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.sut1 = Rectangle(5, 10)
+        self.sut2 = Rectangle(23, 17)
+
+    def test_calc_area(self):
+        self.assertEqual(30, self.sut1.calc_perimeter())
+        self.assertEqual(80, self.sut2.calc_perimeter())
 
 
 if __name__ == '__main__':
